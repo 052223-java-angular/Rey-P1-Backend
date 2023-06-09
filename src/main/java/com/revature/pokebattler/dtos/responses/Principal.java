a87e23404360124df505e2aa261c1b986943fbe0
reyjvergara <rey.j.vergara@gmail.com>
2023-06-08 22:17:40 -0700

    updated the dto response

--- src/main/java/com/revature/pokebattler/dtos/responses/Principal.java
+++ src/main/java/com/revature/pokebattler/dtos/responses/Principal.java
@@ -1,7 +1,24 @@
 package com.revature.pokebattler.dtos.responses;
 
+import com.revature.pokebattler.entities.User;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 public class Principal {
     private String id;
     private String username;
     private String role;
+
+    public Principal(User user){
+        this.id = user.getId();
+        this.username = user.getUsername();
+        this.role = user.getRole().getName();
+    }
 }
